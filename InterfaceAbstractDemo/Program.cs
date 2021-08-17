d362277e05377ede6d4dfc598434c5ca266371dc
Samet Akca <+akcasamet99@users.noreply.github.com>
2021-08-17 13:10:38 +0300

    Add project files.

---
+++ InterfaceAbstractDemo/Program.cs
@@ -0,0 +1,25 @@
+﻿using InterfaceAbstractDemo.Abstract;
+using InterfaceAbstractDemo.Concrete;
+using InterfaceAbstractDemo.Entities;
+using System;
+
+namespace InterfaceAbstractDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BaseCustomerManager customerManager = new NeroCustomerManager();
+            customerManager.Save(new Customer
+            { 
+                Id=1, 
+                DateOfBirth = new DateTime(1999,7,6), 
+                FirstName="Samet"  , 
+                LastName="Akca,", 
+                NationalityId="123456789"  });
+        }
+
+       
+
+    }
+}
